1c608c234a9eece86d20c4006b6930cb2da70504
Meysam03 <alre03001@utb.vaxjo.se>
2023-01-27 10:49:59 +0100

    Add project files.

---
+++ uppgift 5.8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Program
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] siffror = { 12, 384, 36, 21, 46 };
+
+            for(int i = 0; i < siffror.Length; i++)
+            {
+
+                string kråka = siffror[i].ToString();
+
+                char[] fågel = kråka.ToCharArray();
+
+                
+
+                for(int j = 0; j < fågel.Length; j++)
+                {
+                    Thread.Sleep(500);
+                    Console.Write(fågel[j]);   
+                }
+                Console.WriteLine();
+                Thread.Sleep(1000);
+
+            }
+
+
+        }
+    }
+}
+
